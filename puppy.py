11b2442a491b46896ba9841f689d1b9862bd2fcb
Jayasriallam333 <143430488+Jayasriallam333@users.noreply.github.com>
2024-01-12 10:31:05 +0530

    Create puppy.py

---
+++ puppy.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.header('puppy')
+st.write('puppy')
+st.image('https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?cs=srgb&dl=pexels-chevanon-photography-1108099.jpg&fm=jpg',caption = 'puppy')
